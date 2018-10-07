f92cd1edfe398a4b48af12fde957b98423ed560e
terefs <d.van.zurk@gmail.com>
2018-10-07 22:06:06 +0200

    genesis

---
+++ src/Components/Landing/LandingPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class LandingPage extends Component {
+  render() {
+    return (
+    <div style={{width:'100%', margin: 'auto'}}>
+        <div className="landing-banner">
+          <h1>Mass movement</h1>
+          <h6>Planner</h6>
+
+        </div>
+    </div>
+  );
+  }
+}
+
+export default LandingPage
---
+++ src/Components/Login/LoginButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactLoginMS from "react-ms-login";
+
+export default class LoginButton extends React.Component {
+    render() {
+        return (
+            <ReactLoginMS
+                clientId="a157e2ad-7d43-4478-9051-541fd1b2023f" // required: 'application id/client id' obtained from https://apps.dev.microsoft.com for your app
+                redirectUri="http://localhost:9999/authComplete.html" // required: redirectUri registered in https://apps.dev.microsoft.com for your app
+                scopes={["user.read"]} //optional: defaults to "user.read" full list is present https://developer.microsoft.com/en-us/graph/docs/concepts/permissions_reference
+                responseType="token" //optional: valid values are "token" for `Implicite OAuth flow` and "code" for `Authorization Code flow` defaults to "token"
+                cssClass="some-css-class" // optional: space separated class names which are applied on the html Button element
+                btnContent={this.props.label} // optional: can be string or a valid react component which can be rendered inside html Button element
+                handleLogin={(data) => console.log(data)}// required: callback to receive token/code after successful login
+            />
+        )
+    }
+}
